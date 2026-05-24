a38e7c85792c639860b0d5072507db0888ffbb39
Justmore5mins <justmore5mins@gmail.com>
2026-05-24 11:10:28 +0800

    Maybe you can find me?

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ window.showPanel = function(id){
 window.hidePanel = function(){
     document.querySelectorAll(".overlay").forEach(e => e.style.display = "None");
 }
+
+class ContactCard extends HTMLElement{
+    connectedCallback() {
+    const img = this.getAttribute("img");
+    const href = this.getAttribute("href");
+    const name = this.getAttribute("name");
+    const isEnd = this.hasAttribute("end");
+    const description = this.textContent.trim();
+    
+
+    this.innerHTML = `
+      <a href="${href}" target="_blank" rel="noopener noreferrer" class="contact-card">
+        <img src="${img}" alt="${name}">
+        <span>${name}</span>
+        <span id="description">${description}</span>
+        ${!isEnd ? "<hr style='width: 80vw;max-width: 800px; color: #323232;'>" : ""}
+      </a>
+    `;
+  }
+}
+
+customElements.define("custom-contact", ContactCard);
