cb40e71d4f4984b7eecbfbb3c318b7ddae3d0b0e
Justmore5mins <justmore5mins@gmail.com>
2026-05-24 09:26:33 +0800

    Making the 'About Me' block clickable

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 window.onload = function(){
     //----------------------------
-
     document.querySelectorAll("hashtag").forEach(el => el.innerHTML = el.innerHTML.trim().split(" ").map(w => `# ${w}`).join(" "))
+
+    
+}
+
+window.showPanel = function(id){
+    if(!localStorage.getItem(id)){
+        document.getElementById(id).style.display = "flex";
+        localStorage.setItem(id, "showed");
+    }else{
+        hidePanel();
+        localStorage.removeItem(id)
+    }
+}
+
+window.hidePanel = function(){
+    document.querySelectorAll(".overlay").forEach(e => e.style.display = "None");
 }
