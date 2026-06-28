04fa15f8e26f3123646be1915bf71a00e94ee955
Justmore5mins <justmore5mins@gmail.com>
2026-06-29 06:38:49 +0800

    v3

--- script.js
+++ script.js
@@ -1,42 +1,17 @@
-window.onload = function(){
-    //----------------------------
-    document.querySelectorAll("hashtag").forEach(el => el.innerHTML = el.innerHTML.trim().split(" ").map(w => `# ${w}`).join(" "))
-
-    
-}
-
-window.showPanel = function(id){
-    if(!localStorage.getItem(id)){
-        document.getElementById(id).style.display = "flex";
-        localStorage.setItem(id, "showed");
-    }else{
-        hidePanel();
-        localStorage.removeItem(id)
-    }
-}
-
-window.hidePanel = function(){
-    document.querySelectorAll(".overlay").forEach(e => e.style.display = "None");
-}
-
-class ContactCard extends HTMLElement{
-    connectedCallback() {
-    const img = this.getAttribute("img");
-    const href = this.getAttribute("href");
-    const name = this.getAttribute("name");
-    const isEnd = this.hasAttribute("end");
-    const description = this.textContent.trim();
+window.onload = function(){    
     
 
-    this.innerHTML = `
-      <a href="${href}" target="_blank" rel="noopener noreferrer" class="contact-card">
-        <img src="${img}" alt="${name}">
-        <span>${name}</span>
-        <span id="description">${description}</span>
-        ${!isEnd ? "<hr style='width: 80vw;max-width: 800px; color: #323232;'>" : ""}
-      </a>
-    `;
-  }
+    new Typed('#HashTags', {
+        strings: [
+            "#會寫程式的人",
+            "#FRC機器人校隊隊長",
+            "#FRC機器人校隊程式組長",
+            "#把寫程式當成生活的人"
+        ],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true,
+        loopCount: Infinity,
+        showCursor:false
+    });
 }
-
-customElements.define("custom-contact", ContactCard);
